2f28d8c770d2ad99670e354d53edb9482f02a919
Mark <mark.limm@gmail.com>
2021-07-03 17:53:00 -0400

    defining the shared event payload types that are broadcasted from the server

---
+++ lib/types/Events.ts
@@ -0,0 +1,67 @@
+//  these are types that are shared with the frontend.  These types have to do with payloads that get broadcasted to the clients
+
+export enum OkOrError {
+  Ok = 'ok',
+  Error = 'error',
+}
+
+export interface ResponseStatus {
+  error?: string
+  message?: string
+  status: OkOrError
+}
+
+export interface BuildingDetails {
+  elevatorNames: string[]
+  name: string
+  numFloors: number
+  yearBuilt: number
+}
+
+/**
+ * This response is sent when a client first connects to the realtime server
+ */
+export type NewConnectionBuildingResponse = BuildingDetails & ResponseStatus
+
+export interface ElevatorDTO {
+  elevatorId: string
+  name: string
+}
+
+export interface PersonDTO {
+  personId: string
+  name: string
+}
+
+export enum PersonUpdateType {
+  NEWLY_SPAWNED = 'newly-spawned',
+  REQUESTING_ELEVATOR = 'requesting-elevator',
+  ENTERED_THE_ELEVATOR = 'entered-the-elevator',
+  IN_THE_ELEVATOR = 'in-the-elevator',
+  PRESSES_BUTTON = 'presses-button',
+  LEFT_THE_ELEVATOR = 'left-the-elevator',
+}
+
+export enum ElevatorUpdateType {
+  TAKING_REQUEST = 'taking-request',
+  ON_FLOOR = 'on-floor',
+  OPENING_DOORS = 'opening-doors',
+  CLOSING_DOORS = 'closing-doors',
+  MOVING_TO_FLOOR = 'moving-to-floor',
+}
+
+export type PersonUpdate = {
+  type: PersonUpdateType
+
+  person: PersonDTO
+  elevator?: ElevatorDTO
+  currFloor: number
+}
+
+export type ElevatorUpdate = {
+  type: ElevatorUpdateType
+
+  people: PersonDTO[]
+  elevator: ElevatorDTO
+  currFloor: number
+}
